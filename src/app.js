70d7887c784bc5cfd8ab6e23f60e37c853c16e21
keshav kumar <keshavk784@gmail.com>
2020-07-15 02:16:43 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -38,14 +38,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title:'Weather App',
-        name:'Andrew Mead'
+        name:'Keshav kumar'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render("about",{
         title:'About Krishna',
-        name:'Krishna'
+        name:'Keshav kumar'
     })
 })
 
